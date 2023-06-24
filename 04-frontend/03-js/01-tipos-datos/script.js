875bc6e334bcc43e132bcc18329562a7bbbfd635
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-06-24 12:37:01 -0500

    Introduccion a JS - tipos de datos y funciones

---
+++ 04-frontend/03-js/01-tipos-datos/script.js
@@ -0,0 +1,18 @@
+//Tipos de datos primitivos
+37; //Number
+true; //Boolean
+false; //Boolean
+("Hola mundo"); //String
+undefined;
+null;
+
+//Tipos de datos objetos
+[37, true, "Hola"]; //array
+//despues veremos otros tipos de datos...
+
+//Variables
+var nombre = "Fabian";
+var edad = 22;
+
+//Console log para mostrar algo en la consola del navegador
+console.log("Hola mi nombre es: " + nombre + " y tengo " + edad);
