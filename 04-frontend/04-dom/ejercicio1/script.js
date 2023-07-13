92d0cbcb4c346e2b4dc3aab40f8ba8537b5d0ae8
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-07-13 16:32:13 -0500

    Ejercicio 1 - instrucciones

---
+++ 04-frontend/04-dom/ejercicio1/script.js
@@ -0,0 +1,15 @@
+//Actividad
+
+// a) Acceso por etiqueta:
+// Utiliza JavaScript para acceder a todos los elementos <p> en la página y muestra la colección de elementos en la consola del navegador.
+
+// b) Acceso por clase:
+// Utiliza JavaScript para acceder a todos los elementos con la clase "clase1" y muestra la colección de elementos en la consola del navegador.
+
+// c) Acceso por id:
+// Utiliza JavaScript para acceder al elemento con el id "id2" y muestra el elemento en la consola del navegador.
+
+// d) Acceso por selector de CSS:
+// Utiliza JavaScript para acceder a todos los elementos con la clase "clase1" y al elemento con el id "id2" utilizando un selector de CSS combinado. Muestra la colección de elementos en la consola del navegador.
+
+// Recuerda utilizar la consola del navegador para ver los resultados de cada tarea. ¡Diviértete explorando el acceso al DOM!
