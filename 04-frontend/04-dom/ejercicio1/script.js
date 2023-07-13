6922fc2aa541b7ed4685e741ca9f2c40f6e75c1d
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-07-13 17:48:05 -0500

    Leer y crear nodos

--- 04-frontend/04-dom/ejercicio1/script.js
+++ 04-frontend/04-dom/ejercicio1/script.js
@@ -2,14 +2,25 @@
 
 // a) Acceso por etiqueta:
 // Utiliza JavaScript para acceder a todos los elementos <p> en la página y muestra la colección de elementos en la consola del navegador.
+const etiquetasP = document.querySelectorAll("p");
+// Otra opcion con getElementsByTagName
+// const etiquetasP = document.getElementsByTagName("p")
+console.log(etiquetasP);
 
 // b) Acceso por clase:
 // Utiliza JavaScript para acceder a todos los elementos con la clase "clase1" y muestra la colección de elementos en la consola del navegador.
+const elementosConClase = document.querySelectorAll(".clase1");
+// const elementosConClase = document.getElementsByClassName("clase1");
+console.log(elementosConClase);
 
 // c) Acceso por id:
 // Utiliza JavaScript para acceder al elemento con el id "id2" y muestra el elemento en la consola del navegador.
+const parrafoConId = document.querySelector("#id2");
+// const parrafoConId = document.getElementById("id2");
+console.log(parrafoConId);
 
 // d) Acceso por selector de CSS:
 // Utiliza JavaScript para acceder a todos los elementos con la clase "clase1" y al elemento con el id "id2" utilizando un selector de CSS combinado. Muestra la colección de elementos en la consola del navegador.
-
+const elementos = document.querySelectorAll(".clase1, #id2");
+console.log(elementos);
 // Recuerda utilizar la consola del navegador para ver los resultados de cada tarea. ¡Diviértete explorando el acceso al DOM!
--- 04-frontend/04-dom/script.js
+++ 04-frontend/04-dom/script.js
@@ -2,33 +2,61 @@
 
 // 1. getElementById -> Obtener elemento html por su ID
 //Ejemplo 1
-const lorena = document.getElementById("lorena");
+// const lorena = document.getElementById("lorena");
 // console.log(lorena);
 
 //Ejemplo 2
-const dairon = document.getElementById("daironId");
+// const dairon = document.getElementById("daironId");
 // console.log(dairon);
 
 // 2. getElementsByClassName -> Obtener lista de elementos html por su clase
 //Ejemplo 1
-const fabian = document.getElementsByClassName("nombre");
+// const fabian = document.getElementsByClassName("nombre");
 // console.log(fabian);
 
 //Ejemplo 2
-const estudiantes = document.getElementsByClassName("estudiante");
+// const estudiantes = document.getElementsByClassName("estudiante");
 // console.log(estudiantes);
 
 // 3. getElementsByTagName -> Obtener lista de elementos html por el nombre de la etiqueta
 //Ejemplo 1
-const listaEstudiantes = document.getElementsByTagName("li");
+// const listaEstudiantes = document.getElementsByTagName("li");
 // console.log(listaEstudiantes);
 
 //Ejemplo 2
-const titulito = document.getElementsByTagName("h1");
+// const titulito = document.getElementsByTagName("h1");
 
 // 4. querySelector -> Devuelve el primer elemento que coincida con la busqueda, puede ser por nombre de etiqueta, clase o por id
-const lorenaQuerySelector = document.querySelector("#lorena");
+// const lorenaQuerySelector = document.querySelector("#lorena");
 // console.log(lorenaQuerySelector);
 
 // 5. querySelectorAll -> Devuelve todos los elementos que coincidan con la busqueda.
-const estudiantesLista = document.querySelectorAll(".estudiante");
+// const estudiantesLista = document.querySelectorAll(".estudiante");
+
+// NodeList vs Array
+// La diferencia entre un NodeList y Array es que NodeList es una coleccion de nodos, mientras que Array es un objeto que contiene una coleccion de elementos y metodos propios de los array
+
+// 1. Array.from() -> Crea una nueva instancia de Array a partir de un objeto iterable
+// Ejemplo 1
+// const listaEstudiantes = document.querySelectorAll(".estudiante");
+// const listaEstudiantesArray = Array.from(listaEstudiantes);
+
+// Crear elementos
+// 1. createElement -> Crea un elemento HTML (NO LO AGREGA AL HTML)
+// const subtitulo = document.createElement("h3");
+
+// 2. createTextNode -> Crea un nodo de text (NO LO AGREGA AL HTML)
+// const texto = document.createTextNode("Esto es un subtitulo");
+
+// Agregar elementos
+// 1. parenElement.appendChild() -> Agrega un hijo al final del contenedor padre
+// obtengo al elemento padre
+const listaEstudiantes = document.querySelector("ul");
+// creo un nuevo elemento (nodo)
+const andres = document.createElement("li");
+// creo el texto que va a tener el nodo
+const texto = document.createTextNode("Andres");
+// Agregar el texto al nodo (andres)
+andres.appendChild(texto);
+// Agregar el li a la ul
+listaEstudiantes.appendChild(andres);
