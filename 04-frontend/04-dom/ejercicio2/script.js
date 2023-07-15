8f684c6f128e87424dbb5af5d678d153eb35ee05
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-07-15 12:11:58 -0500

    Instrucciones de la actividad

--- 04-frontend/04-dom/ejercicio2/script.js
+++ 04-frontend/04-dom/ejercicio2/script.js
@@ -1,1 +1,17 @@
+/*
+Actividad: Manipulación del DOM con JavaScript
+
+Objetivo: Practicar la manipulación del DOM utilizando JavaScript para agregar elementos HTML.
+
+Instrucciones:
+
+1. Borrar todo el contenido que esta en el HTML
+2. Crea los elementos HTML y agrega el mismo contenido HTML que se proporcionó anteriormente en la actividad sobre gatos.
+3. Utiliza los métodos y propiedades del DOM para agregar los elementos creados al elemento <div> vacío.
+
+Nota: Recuerda comentar tu código adecuadamente para explicar cada paso y facilitar su comprensión.
+
+¡Diviértete explorando la manipulación del DOM con JavaScript!
+
+*/
 // Tu codigo aqui:
