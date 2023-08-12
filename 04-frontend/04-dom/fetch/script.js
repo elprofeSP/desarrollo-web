fb0ca630b775c1ab2a4e8dd182f426129c3d01de
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-08-12 09:55:22 -0500

    Fetch + DOM parte 1

--- 04-frontend/04-dom/fetch/script.js
+++ 04-frontend/04-dom/fetch/script.js
@@ -1,7 +1,30 @@
-const url = "https://jsonplaceholder.typicode.com/comments/1";
+const url = "https://jsonplaceholder.typicode.com/comments/1000";
+const comentarios = document.querySelector(".comentarios");
+
 fetch(url)
-  .then((respuesta) => respuesta.json())
-  .then((data) => {
-    //codigo aqui
+  .then((respuesta) => {
+    if (!respuesta.ok) {
+      throw new Error("No se ha podido conectar");
+    } else {
+      return respuesta.json();
+    }
   })
-  .catch((error) => console.log(error));
+  .then((data) => {
+    mostrarComentario(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+function mostrarComentario(data) {
+  const comentario = document.createElement("div");
+  comentario.className = "comentario";
+  const h2 = document.createElement("h2");
+  h2.textContent = data.name;
+  const h3 = document.createElement("h3");
+  h3.textContent = data.email;
+  const p = document.createElement("p");
+  p.textContent = data.body;
+  comentario.append(h2, h3, p);
+  comentarios.append(comentario);
+}
